066a3d5b0f213d5502df472c8167a5ae55552f16
PratyushAnand08 <PratyushAnand08@gmail.com>
2016-12-13 00:35:04 -0500

    Initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/pratyush1/Documents/NYU/Projects/ITP_Fall16/archemy/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
